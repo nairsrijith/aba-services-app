ed6cf3beb2ada5a336025ea74cce0a334fc010f9
vsrijith <nair.srijith@gmail.com>
2025-09-06 19:33:13 -0400

    changed the sidebar and bottom navbar. Bottom navbar still have overlay issues.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-# from datetime import date
+from datetime import date
 
 
 @login_manager.user_loader
