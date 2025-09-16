0a42be985cd9316cad84bb1067b097825ef20af6
vsrijith <nair.srijith@gmail.com>
2025-09-15 20:49:58 -0400

    correct the category field in the activity form

--- app/manage/forms.py
+++ app/manage/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, StringField
+from wtforms import SubmitField, StringField, SelectField
 from wtforms.validators import DataRequired, length
 from app.models import User
 
@@ -11,5 +11,5 @@ class DesignationForm(FlaskForm):
 
 class ActivityForm(FlaskForm):
     name = StringField('Activity Name', validators=[DataRequired(), length(max=51)])
-    category = StringField('Category', validators=[DataRequired(), length(max=51)])
+    category = SelectField('Category', validators=[DataRequired(), length(max=51)])
     submit = SubmitField('Add Activity')
